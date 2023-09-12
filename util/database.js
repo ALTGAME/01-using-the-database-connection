8874e215e69f6244802f6e812b42fdf2f4f1cbfb
ALTGAME <126790140+ALTGAME@users.noreply.github.com>
2023-09-12 18:24:23 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://krgpsh13:9i078VgjEvbVX1od@cluster0.7shea1k.mongodb.net/?retryWrites=true&w=majority'
+  
   )
     .then(client => {
       console.log('Connected!');
